99d091eb975d001bbddee11ed69bc1cd510fcd5a
Arrow <arrowthefighter@gmail.com>
2026-03-02 07:57:27 +0100

    updated cooler model

---
+++ Scripts/cooler_visuals_script.gd
@@ -0,0 +1,38 @@
+extends Node
+
+@export var machine : Machine
+@export var start_pos : Node3D
+@export var middle_pos : Node3D
+@export var end_pos : Node3D
+@export var animation_player: AnimationPlayer 
+
+func _ready() -> void:
+    machine.input_started.connect(input_started)
+    machine.output_started.connect(output_started)
+    machine.process_started.connect(process_started)
+    machine.process_finished.connect(process_ended)
+    pass
+    
+    
+func input_started(item_number):
+    var tween = create_tween()
+    #item_parent.global_position = start_pos.global_positionsd
+    tween.tween_property(machine.display_points[item_number],"global_position",middle_pos.global_position,machine.input_duration)
+    
+    pass
+    
+func process_started():
+    if animation_player != null:
+        animation_player.play("anim_Mixer_Middle")
+    pass
+    
+func process_ended(ingredients):
+    if animation_player != null:
+        animation_player.pause()
+    pass
+    
+func output_started():
+    var tween = create_tween()
+    tween.tween_property(machine.display_points[0],"global_position",end_pos.global_position,machine.output_duration)
+    
+    pass
--- Scripts/ui_machine_tab_script.gd
+++ Scripts/ui_machine_tab_script.gd
@@ -3,11 +3,13 @@ extends PanelContainer
 @onready var machine_price: Label = $"ui_mcahine_tab/HBoxContainer/VBoxContainer/Machine Price"
 @onready var machine_description: Label = $ui_mcahine_tab/HBoxContainer/MachineDescription
 @onready var ui_mcahine_tab: Button = $ui_mcahine_tab
+@onready var machine_icon: TextureRect = $ui_mcahine_tab/HBoxContainer/Panel/MachineIcon
 
 signal machine_tab_pressed(number)
 var machine_number
 
 func setup_tab(machine : Machine,number):
+    machine_icon.texture = machine.machine_icon
     ui_mcahine_tab.pressed.connect(tab_pressed)
     machine_number = number
     
